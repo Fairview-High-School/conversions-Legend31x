5baeeb639c1e1473f9e6e59da8ef09c6e742bbdf
Legend31x <85188726+Legend31x@users.noreply.github.com>
2024-08-27 16:59:16 +0000

    final

--- Conversions.java
+++ Conversions.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 /**
  * This application converts between various units of measurement.
  *
- * @author Tim Gesell
- * @version 8/21/2022
+ * @author Nate Gill
+ * @version 8/23/2024
  */
 public class Conversions
 {
@@ -17,6 +17,10 @@ public class Conversions
         System.out.println("4. Meters to Feet");
         System.out.println("5. Ounces to Milliliters");
         System.out.println("6. Milliliters to Ounces");
+        System.out.println("7. Robux to USD");
+        System.out.println("8. USD to Robux");
+        System.out.println("9. Dollars to 1989 Nissan S Cargo");
+        System.out.println("10. 1989 Nissan S Cargo to Dollars");
         //Add 2 more conversions, each both ways, to add choices 7, 8, 9, and 10.
         //You can add even more if you would like.
 
@@ -33,8 +37,78 @@ public class Conversions
         }
         if (selection == 2)
         {
-            //add the code to ask the user to enter Fahrenheit and then convert to Celsius
+            System.out.println("Enter Fahrenheit: ");
+            double fahrenheit = keyboard.nextDouble();
+            keyboard.nextLine();
+            double celsius = (fahrenheit-32) / (9.0/5.0);
+            System.out.println(fahrenheit + " degrees fahrenheit is " + celsius + " degrees celsiuzs");
         }
-        //add more if statements here.
+        if (selection == 3)
+        {
+            System.out.println("Enter Feet: ");
+            double feet = keyboard.nextDouble();
+            keyboard.nextLine();
+            double meters = feet * 3.281;
+            System.out.println(feet + " degrees Feet is " + meters + " degrees Meters");
+        }
+        if (selection == 4)
+        {
+            System.out.println("Enter Meters: ");
+            double meters = keyboard.nextDouble();
+            keyboard.nextLine();
+            double div = 3.281;
+            double feet = meters / div;
+            System.out.println(meters + " Meters is " + feet + " feet");
+        }
+        if (selection == 5)
+        {
+            System.out.println("Enter Meters: ");
+            double meters = keyboard.nextDouble();
+            keyboard.nextLine();
+            double div = 3.281;
+            double feet = meters / div;
+            System.out.println(meters + " Meters is " + feet + " feet");
+        }
+        if (selection == 6)
+        {
+            System.out.println("Enter Meters: ");
+            double meters = keyboard.nextDouble();
+            keyboard.nextLine();
+            double div = 3.281;
+            double feet = meters / div;
+            System.out.println(meters + " Meters is " + feet + " feet");
+        }
+        if (selection == 7)
+        {
+            System.out.println("Enter Robux: ");
+            double robux = keyboard.nextDouble();
+            keyboard.nextLine();
+            double dollars = robux / 100;
+            System.out.println(robux + " Robux is " + dollars + " Dollars");
+        }
+        if (selection == 8)
+        {
+            System.out.println("Enter Dollars: ");
+            double dollars = keyboard.nextDouble();
+            keyboard.nextLine();
+            double robux = dollars * 100;
+            System.out.println(dollars + " Dollars is " + robux + " Robux");
+        }
+        if (selection == 9)
+        {
+            System.out.println("Enter Dollars: ");
+            double dollars = keyboard.nextDouble();
+            keyboard.nextLine();
+            double nissan = dollars / 17000;
+            System.out.println(dollars + " Dollars is " + nissan + " 1989 Nissan S Cargos");
+        }
+        if (selection == 10)
+        {
+            System.out.println("Enter 1989 Nissan S Cargos: ");
+            double nissan = keyboard.nextDouble();
+            keyboard.nextLine();
+            double dollars = nissan * 17000;
+            System.out.println(nissan + " 1989 Nissan S Cargos is " + dollars + " Dollars");
+        }
     }
 }
